851aa4972b041917216d33f95d8112cf6bc7b47d
Sam Hamm <samhamm@gmail.com>
2017-06-22 23:11:22 -0700

    order form now stores data in LS

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
 'use strict';
 
 var form = document.getElementById('orderform');
+Order.all = [];
 
-function Order(){
-  this.
+function Order(selection, quantity, name, street, city, state, zip, phone, email){
+  this.selection = selection;
+  this.quantity = quantity;
+  this.name = name;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
+  this.phone = phone;
+  this.email = email;
+  Order.all.push(this);
 }
+
 
 function handleForm(e){
   e.preventDefault();
   // console.log(e.target.quantity.value)
-  new Order();
+  new Order(
+    event.target.selection.value,
+    event.target.quantity.value,
+    event.target.name.value,
+    event.target.street.value,
+    event.target.city.value,
+    event.target.state.value,
+    event.target.zip.value,
+    event.target.phone.value,
+    event.target.email.value);
+  console.table(Order.all);
+
+  localStorage.orders = JSON.stringify(Order.all);
+
+  var names = ['selection','quantity','name','street','city','state','zip','phone','email'];
+  for (var i = 0; i < names.length; i++){
+    event.target[names[i]].value = null;
+  }
+
 }
 
-form.addEventListener('submit', handleForm)
+if(localStorage.orders) {
+  Order.all = JSON.parse(localStorage.orders);
+}
+
+form.addEventListener('submit', handleForm);
